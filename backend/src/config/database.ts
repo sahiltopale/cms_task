1c96391bae81b52a59b7f129e33fdfd285863c29
Sahil Topale <sahil.topale2005@student.sfit.ac.in>
2026-06-09 23:52:14 +0530

    Added README

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,16 +3,15 @@ dotenv.config();
 
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || "cms_db",
-  process.env.DB_USER || "postgres",
-  process.env.DB_PASSWORD || "",
-  {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: "postgres",
-    logging: false,
+const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
-);
+});
 
 export default sequelize;
