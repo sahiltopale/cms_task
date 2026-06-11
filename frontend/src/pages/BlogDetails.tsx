e582f5f1184fdfb019ba895ac7482c48c571d5b4
Sahil Topale <sahil.topale2005@student.sfit.ac.in>
2026-06-11 18:03:38 +0530

    Changes Made by Sahil

--- frontend/src/pages/BlogDetails.tsx
+++ frontend/src/pages/BlogDetails.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../api/axios";
-import "../styles/blogDetails.css";
+import "../styles/blogdetails.css";
 
 function BlogDetails() {
   const { slug } = useParams();
